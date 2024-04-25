016c39ce131e85918a90f5c94b1c96a1a50e317d
kalyangonuguntla <155189837+kalyangonuguntla@users.noreply.github.com>
2024-04-25 14:57:10 +0100

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -1,13 +1,14 @@
+// src/main/java/com/example/demo/DemoApplication.java
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-public class HelloController {
+@SpringBootApplication
+public class DemoApplication {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from Jenkins CI/CD!";
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
+
 }
